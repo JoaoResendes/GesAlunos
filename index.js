c4300ece610d932448344a8dde5a8987309bc829
JoaoResendes <93650121+JoaoResendes@users.noreply.github.com>
2021-11-29 12:40:42 +0000

    aula de dia 29 de Novembro

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 5000;
+
+
+//cliente so tem acesso ao public
+app.use(express.static('./public'))
+
+// pedido em http://localhost:5000/
+app.get('/',(req, res) => {
+ res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+
+app.listen(PORT, function(){
+    console.log("Server is running on PORT " + PORT);
+ });
